38725c9eb2351c5a8d08f9203972cabd2ecdd4ab
SergeiShein <sergeyshein20@yandex.ru>
2023-07-25 09:30:39 +0300

    first commit

---
+++ src/test/java/TestAddCourier.java
@@ -0,0 +1,110 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.*;
+
+import java.io.File;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+public class TestAddCourier {
+    @BeforeClass
+    public static void setUp() {
+        RestAssured.baseURI = "http://qa-scooter.praktikum-services.ru/";
+    }
+    @Test
+    public void createNewCourier(){
+        File json = new File("src/test/resources/Courier/valideCourier.json");
+        Response response =
+                given()
+                        .header("Content-type", "application/json")
+                        .and()
+                        .body(json)
+                        .when()
+                        .post("/api/v1/courier");
+        System.out.println(response.getBody().asString());
+        response.then().assertThat().body("ok", equalTo(true))
+                .and()
+                .statusCode(201);
+    }
+    @Test
+    public void createRepeatCourier(){
+        File json = new File("src/test/resources/Courier/valideSecondCourier.json");
+        Response response =
+                given()
+                        .header("Content-type", "application/json")
+                        .and()
+                        .body(json)
+                        .when()
+                        .post("/api/v1/courier");
+        Response repeatResponse =
+                given()
+                        .header("Content-type", "application/json")
+                        .and()
+                        .body(json)
+                        .when()
+                        .post("/api/v1/courier");
+        System.out.println(repeatResponse.getBody().asString());
+        repeatResponse.then().assertThat().body("message", equalTo("Этот логин уже используется. Попробуйте другой."))
+                .and()
+                .statusCode(409);
+    }
+    @Test
+    public void createInvalidCourier(){
+        File json = new File("src/test/resources/Courier/invalideCourier.json");
+        Response response =
+                given()
+                        .header("Content-type", "application/json")
+                        .and()
+                        .body(json)
+                        .when()
+                        .post("/api/v1/courier");
+        System.out.println(response.getBody().asString());
+        response.then().assertThat().body("message", equalTo("Недостаточно данных для создания учетной записи"))
+                .and()
+                .statusCode(400);
+    }
+    @Test
+    public void createInvalidSecondCourier(){
+        File json = new File("src/test/resources/Courier/invalideSecondCourier.json");
+        Response response =
+                given()
+                        .header("Content-type", "application/json")
+                        .and()
+                        .body(json)
+                        .when()
+                        .post("/api/v1/courier");
+        System.out.println(response.getBody().asString());
+        response.then().assertThat().body("message", equalTo("Недостаточно данных для создания учетной записи"))
+                .and()
+                .statusCode(400);
+    }
+    @AfterClass
+    public static void deleteCourier(){
+        File json = new File("src/test/resources/Courier/valideCourier.json");
+        Response response =
+                given()
+                        .header("Content-type", "application/json")
+                        .and()
+                        .body(json)
+                        .when()
+                        .post("/api/v1/courier/login");
+        Integer id = response.path("id");
+        given()
+                .header("Content-type", "application/json")
+                .when()
+                .delete("/api/v1/courier/" + id);
+        File secondJson = new File("src/test/resources/Courier/valideSecondCourier.json");
+        Response secondResponse =
+                given()
+                        .header("Content-type", "application/json")
+                        .and()
+                        .body(secondJson)
+                        .when()
+                        .post("/api/v1/courier/login");
+        Integer secondId = secondResponse.path("id");
+        given()
+                .header("Content-type", "application/json")
+                .when()
+                .delete("/api/v1/courier/" + secondId);
+    }
+}
---
+++ src/test/java/TestAddOrder.java
@@ -0,0 +1,49 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.io.File;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.notNullValue;
+
+@RunWith(Parameterized.class)
+
+public class TestAddOrder {
+    private final String jsonName;
+    public TestAddOrder(String jsonName) {
+        this.jsonName = jsonName;
+    }
+    @Before
+    public void setUp() {
+        RestAssured.baseURI = "http://qa-scooter.praktikum-services.ru/";
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] getCredentials() {
+        return new Object[][]{
+                {"oneBlackColourOrder.json"},
+                {"oneGreyColourOrder.json"},
+                {"twoColourOrder.json"},
+                {"withoutColourOrder.json"},
+        };
+    }
+    @Test
+    public void testOrder() {
+        File json = new File("src/test/resources/Order/" + jsonName);
+        Response response =
+                given()
+                        .header("Content-type", "application/json")
+                        .and()
+                        .body(json)
+                        .when()
+                        .post("/api/v1/orders");
+        System.out.println(response.getBody().asString());
+        response.then().assertThat().body("track",  notNullValue())
+                .and()
+                .statusCode(201);
+    }
+}
